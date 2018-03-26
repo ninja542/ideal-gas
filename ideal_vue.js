71bc3bed0a47d435268b8841e6b09b0c13f33cab
ninja542 <theroboticcyborg@gmail.com>
2018-03-26 00:20:21 -0400

    particle tracking with different colors for different speeds

--- ideal_vue.js
+++ ideal_vue.js
@@ -7,6 +7,7 @@ var animate = window.requestAnimationFrame ||
 function randNeg(){
 	return Math.floor(Math.random()*2) == 1 ? 1 : -1;
 }
+
 // object definition
 function Ball(x, y){
 	this.x = x;
@@ -60,11 +61,17 @@ Ball.prototype.bounce = function(p, i){
 };
 
 var canvas = document.getElementById("canvas");
+var canvas2 = document.getElementById("canvas2");
 var width = 400;
 var height = 600;
 canvas.width = width;
 canvas.height = height;
 var context = canvas.getContext('2d');
+canvas2.width = width;
+canvas2.height = height;
+var context2 = canvas2.getContext('2d');
+
+let colorscale = d3.scaleSequential(d3.interpolateWarm);
 
 var app = new Vue({
 	el: "#app",
@@ -73,6 +80,7 @@ var app = new Vue({
 		particles: [],
 		width: 400,
 		height: 600,
+		track_particle: false,
 	},
 	methods: {
 		adjustParticles: function(){
@@ -95,6 +103,14 @@ var app = new Vue({
 			this.particles.forEach(p => context.clearRect(p.x-10, p.y-10, 20, 20));
 			this.particles.forEach(p => p.update());
 			this.particles.forEach((p,i) => p.bounce(p, i));
+			if(this.track_particle == true){
+				context2.beginPath();
+				context2.arc(this.particles[0].x, this.particles[0].y, 2, 0, 2*Math.PI, false);
+				// context2.fillStyle = "#FF0000";
+				let speed = Math.sqrt(Math.pow(this.particles[0].x_speed, 2)) + Math.pow(this.particles[0].y_speed, 2);
+				context2.fillStyle = colorscale((speed/10 < 1) ? speed/10 : 1);
+				context2.fill();
+			}
 		},
 		step: function(){
 			this.update();
@@ -108,10 +124,19 @@ var app = new Vue({
 		width: function(){
 			canvas.width = this.width;
 			canvas.style.width = this.width;
+
+			canvas2.width = this.width;
+			canvas2.style.width = this.width;
 		},
 		height: function(){
 			canvas.height = this.height;
 			canvas.style.height = this.height;
+
+			canvas2.height = this.height;
+			canvas2.style.height = this.height;
+		},
+		track_particle: function(){
+			context2.clearRect(0, 0, this.width, this.height);
 		}
 	},
 	mounted: function(){
@@ -119,7 +144,7 @@ var app = new Vue({
 			this.particles.push(new Ball(Math.floor(Math.random()*width), Math.floor(Math.random()*height)));
 		}
 		this.canvasRender();
-	}
+	},
 });
 var step = function(){
 	app.update();
